9d859b355ff72277c504745a79b4220da6b981d3
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-09-08 13:48:11 +0700

    props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,18 +1,27 @@
 const Hello = (props) => {
+
+  console.log(props)
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
 const App = () => {
+
+  const name = 'Peter'
+  const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
 
-      <Hello name='George' />
-      <Hello name='Juliet' />
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
